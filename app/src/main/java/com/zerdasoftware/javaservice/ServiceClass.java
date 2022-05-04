dfa09007d2ae65757dcaff6f9d7650210b5755be
cihancalli <cihan.callii@gmail.com>
2022-05-04 19:28:16 +0300

    Async Task

--- app/src/main/java/com/zerdasoftware/javaservice/ServiceClass.java
+++ app/src/main/java/com/zerdasoftware/javaservice/ServiceClass.java
@@ -2,6 +2,7 @@ package com.zerdasoftware.javaservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -37,11 +38,55 @@ public class ServiceClass extends Service {
         String currentThread = Thread.currentThread().getName();
         Log.d(TAG, "onStartCommand: " + currentThread);
 
+        /*
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+         */
+
+        AsyncTaskClass asyncTaskClass = new AsyncTaskClass();
+        asyncTaskClass.execute(10000);
+
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    class AsyncTaskClass extends AsyncTask<Integer,Void,Void> {
+        private static final String TAG = "AsyncTaskClass";
+        @Override
+        protected Void doInBackground(Integer... integers) {
+            String currentThread = Thread.currentThread().getName();
+            Log.d(TAG, "doInBackground: " + currentThread);
+            int sleepTime = integers[0];
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            String currentThread = Thread.currentThread().getName();
+            Log.d(TAG, "onPreExecute: " + currentThread);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            String currentThread = Thread.currentThread().getName();
+            Log.d(TAG, "onPostExecute: " + currentThread);
+            super.onPostExecute(unused);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            String currentThread = Thread.currentThread().getName();
+            Log.d(TAG, "onProgressUpdate: " + currentThread);
+            super.onProgressUpdate(values);
+        }
     }
 }
